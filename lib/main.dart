a872da72697b6ebbbd36a4b2a313b17963ff3475
DevAppsGam <devapps@asesoresgam.com.mx>
2023-06-19 14:05:05 -0600

    V1_1_0 Pantalla de carga previa, y login estetica modificado

--- lib/main.dart
+++ lib/main.dart
@@ -7,26 +7,81 @@ import 'Pages/operacionesPage.dart';
 import 'Pages/gddsPage.dart';
 import 'Pages/powerPage.dart';
 import 'Pages/promocionesPage.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() => runApp(const LoginApp());
+void main() => runApp(const MyApp());
 
-class LoginApp extends StatelessWidget {
-  const LoginApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'GAM LOGIN',
-      home: const LoginPage(),
+      home: const SplashScreen(),
       routes: {
         '/PowerPage': (_) => Power(),
         '/asesoresPage': (_) => Asesores(),
-        '/LoginPage': (_) => LoginPage(),
+        '/LoginPage': (_) => const LoginPage(),
         '/promocionesPage': (_) => Promociones(),
         '/gddsPage': (_) => Gdds(),
         '/operacionesPage': (_) => Operaciones(),
       },
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _loadSplashScreen();
+  }
+
+  Future<void> _loadSplashScreen() async {
+    await Future.delayed(const Duration(seconds: 7));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/img/back.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                //LOGO PANTALLA CARGA
+                'assets/img/LOGOGAM.png',
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(
+                color: Color.fromRGBO(250, 161, 103, 2),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
@@ -43,13 +98,6 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController controllerPass = TextEditingController();
   String mensaje = '';
 
-  @override
-  void dispose() {
-    controllerUser.dispose();
-    controllerPass.dispose();
-    super.dispose();
-  }
-
   Future<void> login() async {
     final response = await http.post(
       Uri.parse("http://192.168.1.98/gam/login.php"),
@@ -60,7 +108,7 @@ class _LoginPageState extends State<LoginPage> {
     );
 
     final Map<String, dynamic> datauser =
-        jsonDecode(response.body) as Map<String, dynamic>;
+    jsonDecode(response.body) as Map<String, dynamic>;
     if (datauser.isEmpty) {
       setState(() {
         mensaje = "USUARIO O CONTRASEÑA INCORRECTA";
@@ -70,7 +118,7 @@ class _LoginPageState extends State<LoginPage> {
       switch (userType) {
         case 'admin':
           if (mounted) {
-            Navigator.pushReplacementNamed(context, '/PowerPage');
+            Navigator.pushReplacementNamed(context, '/Power');
           }
           break;
         case 'promocion':
@@ -107,125 +155,153 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/img/back.jpg"),
-            fit: BoxFit.cover,
+      body: Form(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/img/back.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              const Expanded(
-                flex: 2,
-                child: SizedBox(),
-              ),
-              Expanded(
-                flex: 3,
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    "assets/img/LOGOGAM.png",
-                    width: 170,
-                    height: 170,
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(height: 100),
+                Image.asset(
+                  'assets/img/IntraGAM.png',
+                  width: 170.0,
+                  height: 250.0,
+                ),
+                //MENSAJE BIENVENIDO
+                const SizedBox(height: 20),
+                const Text(
+                  '!BIENVENIDO!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(73, 78, 84, 1),
                   ),
                 ),
-              ),
-              Expanded(
-                flex: 4,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
+                const SizedBox(height: 50),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  padding: const EdgeInsets.only(
+                    top: 4,
+                    left: 16,
+                    right: 16,
+                    bottom: 4,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    color: Colors.transparent,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    controller: controllerUser,
+                    decoration: const InputDecoration(
+                      hintText: 'USUARIO',
+                      icon: Icon(
+                        Icons.person_2_rounded,
+                        color: Colors.black,
+                      ),
+                      fillColor: Colors.transparent,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 32),
+                  padding: const EdgeInsets.only(
+                    top: 4,
+                    left: 16,
+                    right: 16,
+                    bottom: 4,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                    color: Colors.transparent,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: TextField(
+                    controller: controllerPass,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      icon: Icon(
+                        Icons.vpn_key,
+                        color: Colors.black,
+                      ),
+                      hintText: 'CONTRASEÑA',
+                      fillColor: Colors.transparent,
+                      filled: true,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    const url = 'https://asesoresgam.com.mx/aviso-de-privacidad.php';
+                    launch(url); // Abrir el enlace en el navegador del dispositivo
+                  },
+                  child: const Text(
+                    'Aviso de privacidad',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: const Color.fromRGBO(242, 115, 23, 1),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  onPressed: () {
+                    login();
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'Ingresar',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Container(
-                        padding: const EdgeInsets.only(bottom: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: const Color.fromRGBO(246, 239, 111, 2),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 5,
-                            ),
-                          ],
-                        ),
-                        child: TextField(
-                          controller: controllerUser,
-                          decoration: const InputDecoration(
-                            hintText: 'USUARIO',
-                            prefixIcon: Icon(
-                              Icons.person_2_rounded,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      Container(
-                        padding: const EdgeInsets.only(bottom: 16),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          color: const Color.fromRGBO(246, 239, 111, 2),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 5,
-                            ),
-                          ],
-                        ),
-                        child: TextField(
-                          controller: controllerPass,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(
-                              Icons.vpn_key,
-                              color: Colors.black,
-                            ),
-                            hintText: 'CONTRASEÑA',
-                          ),
-                        ),
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Padding(
-                          padding: const EdgeInsets.only(right: 32),
-                          child: Text(
-                            'RECUPERAR MI CONTRASEÑA',
-                            style: TextStyle(
-                              color: const Color.fromRGBO(140, 169, 209, 2),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const Spacer(),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: const Color(0xFF0270CE),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                        ),
-                        onPressed: () {
-                          login();
-                        },
-                        child: const Text('Ingresar'),
-                      ),
-                      const SizedBox(height: 16),
-                      Text(
-                        mensaje,
-                        style: const TextStyle(
-                          fontSize: 25,
-                          color: Colors.red,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const SizedBox(height: 50),
+                      const Text(
+                        '© 2019 Grupo Administrativo Mexicano S.A de C.V | Todos los derechos reeservados',
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Color.fromRGBO(167, 168, 160, 1),
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
